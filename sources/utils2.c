966e0b37d7e6d49d1e1f90f01a00a84e2ab15ab0
Ugo Zanchi <uzanchi@made-f0Br8s2.clusters.42paris.fr>
2024-10-16 14:06:33 +0200

    utils

--- sources/utils2.c
+++ sources/utils2.c
@@ -6,7 +6,7 @@
 /*   By: uzanchi <uzanchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 13:12:28 by uzanchi           #+#    #+#             */
-/*   Updated: 2024/10/16 13:32:45 by uzanchi          ###   ########.fr       */
+/*   Updated: 2024/10/16 14:06:25 by uzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,19 @@ int    ft_max(int a, int b)
     if (a >= b)
         return (a);
     return (b);
+}
+
+clock_t get_time(void)
+{
+    struct timeval  time;
+    clock_t         output;
+
+    gettimeofday(&time, NULL);
+	output = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	return (output);
+}
+
+int display_log(char *log, t_philo *philo)
+{
+    
 }
